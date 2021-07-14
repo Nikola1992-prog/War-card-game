31036850014c13eb6d6d58e6c9dc918087f7ad83
Nikola1992-prog <nikola.avramovic11420@gmail.com>
2021-07-14 16:32:13 +0200

    Logic for adding all 52 cards (instances of card class obj) to the deck.

--- War game.py
+++ War game.py
@@ -26,3 +26,8 @@ class Deck():
 
     def __init__(self):
         self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit, rank)
+                self.all_cards.append(created_card)
