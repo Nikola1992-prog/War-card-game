d0f6c92fdfbe0470984bb55e293d1a37e3b451cf
Nikola1992-prog <nikola.avramovic11420@gmail.com>
2021-07-14 15:57:30 +0200

    added Two Global variables for card suits and ranks

--- War game.py
+++ War game.py
@@ -1,5 +1,6 @@
-# Card class
-# card suit, card rank, card value
+# Card suits and Ranks
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 
 # Card values
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
