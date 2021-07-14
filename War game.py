d099f99bb4ddab28fb6213b7af70f37058774574
Nikola1992-prog <nikola.avramovic11420@gmail.com>
2021-07-14 15:41:42 +0200

    integrated global variable values, into Card class to work interactively like Card class attribute

--- War game.py
+++ War game.py
@@ -10,6 +10,7 @@ class Card():
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+        self.value = values[rank]
 
     def __str__(self):
         return self.rank + " of " + self.suit
